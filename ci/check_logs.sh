c46efd074ad851a067c99243c65578a50c0415ae
Marcel Frei <marcel.frei@electronix.ch>
2019-05-27 13:32:23 +0200

    check_logs: better detection of error or warning

--- ci/check_logs.sh
+++ ci/check_logs.sh
@@ -8,11 +8,11 @@ set -x
 
 for file in *.log; do
     for entry in "$(basename "$file")"; do
-        if grep -q error $entry; then
+        if grep -q error: $entry; then
             echo "error(s) found in $file"
             exit 1
         fi
-        if grep -q warning $entry; then
+        if grep -q warning: $entry; then
             echo "warning(s) found in $file"
             exit 1
         fi
